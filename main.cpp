7851c2e9996a3b63fe231b5da0ae7f53249ab656
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-16 19:44:58 +0200

    main

--- main.cpp
+++ main.cpp
@@ -12,7 +12,23 @@ int main() {
     //strcpy_s(a.name, 50, "Pupkin"); //VS
     strcpy(a.name, "Pupkin"); //CLion, Online
 
-    a.showInfo();
-    cout << sizeof(a) << endl;
+    //a.showInfo();
+    //cout << sizeof(a) << endl;
 
+    Student b = {"Ivanchuk", 7, 8, 12};
+    //b.showInfo();
+
+    int N = 2;
+    Studen* arr = new Studen[N] {
+        {"Sydorchuk", 10, 6, 11},
+        {"Shevchenko", 3, 5, 2}
+    };
+
+    addItemBack(arr, N, a);
+    addItemBack(arr, N, b);
+    //N => 4
+    for (int i = 0; i < N; i++) {
+        //arr[i].showInfo();
+        cout << i+1<< ". " << arr[i].name << endl;
+    }
 }
